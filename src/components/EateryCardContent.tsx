24657b6be9b9967e3ba2e51c8f1e151434e444f2
topshenyi-web <erxu@ctemc.org>
2026-03-14 16:28:45 -0400

    feat: add notice for gallo

--- src/components/EateryCardContent.tsx
+++ src/components/EateryCardContent.tsx
@@ -1,10 +1,15 @@
 import { Pin, MapPin } from 'lucide-react';
+import { Interval } from 'luxon';
 import { ILocation_Full } from '../types/locationTypes';
 import css from './EateryCardContent.module.css';
+import { useCurrentTime } from '../contexts/NowContext';
 
 function EateryCardContent({ location, partOfMainGrid }: { location: ILocation_Full; partOfMainGrid: boolean }) {
     const { location: physicalLocation, name, cardViewPreference } = location;
     const isPinned = cardViewPreference === 'pinned';
+    const now = useCurrentTime();
+    const interval = Interval.fromDateTimes(now, { year: 2026, month: 5, day: 5 });
+    const isGallo = location.name.toLowerCase().includes('gallo');
 
     return (
         <div className={css['card-content-container']}>
@@ -13,10 +18,16 @@ function EateryCardContent({ location, partOfMainGrid }: { location: ILocation_F
                 <span>{name}</span>
             </h3>
 
-            <span className={css['physical-location-text']}>
-                <MapPin size={14} />
-                <span>{physicalLocation}</span>
-            </span>
+            {isGallo && interval.isValid ? (
+                <span className={`${css['physical-location-text']} ${css['physical-location-text--warn']}`}>
+                    Permanently closing in {interval.count('day')} days
+                </span>
+            ) : (
+                <span className={css['physical-location-text']}>
+                    <MapPin size={14} />
+                    <span>{physicalLocation}</span>
+                </span>
+            )}
 
             {partOfMainGrid && (
                 <div className={css['action-bar']}>
